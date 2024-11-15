c48ae70c470dfc6f9799b32c1e0e5a7f3d72d67e
liudawei <liudawei@icsoc.net>
2024-11-15 14:54:53 +0800

    feat: setState是否是异步的？

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,24 +1,80 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { ThemeContext } from '../context/Theme'
+import { flushSync } from 'react-dom'
 
 class Header extends Component {
   // 类组件无法关联多个Context
   static contextType = ThemeContext
   static propTypes = {
-    list: PropTypes.array
+    list: PropTypes.array,
   }
 
   static defaultProps ={
     list: []
   }
 
+  state = {
+    counter: 0
+  }
+
+  add = ()=>{
+    // setState是异步的，最终counter只加了 1
+    // this.setState({
+    //   counter: this.state.counter+1
+    // })
+    // this.setState({
+    //   counter: this.state.counter+1
+    // })
+    // this.setState({
+    //   counter: this.state.counter+1
+    // })
+
+    // 函数时，最终counter只加了 3
+    // this.setState((state)=>{
+    //   return {
+    //     counter: state.counter +1
+    //   }
+    // })
+    // this.setState((state)=>{
+    //   return {
+    //     counter: state.counter +1
+    //   }
+    // })
+    // this.setState((state)=>{
+    //   return {
+    //     counter: state.counter +1
+    //   }
+    // })
+
+    // react18之前，同步的，counter加了3 ; 18之后最终counter只加了 1
+    // 如果想要让某一次setState变成同步的，可以使用 flushSync 包裹
+    setTimeout(()=>{
+      flushSync(()=>{
+        this.setState({
+          counter: this.state.counter+1
+        })
+      })
+      this.setState({
+        counter: this.state.counter+1
+      })
+      this.setState({
+        counter: this.state.counter+1
+      })
+    },0)
+  }
+
+
   render() {
     const { list } = this.props
+    const { counter} = this.state
     return (
       <div>
         <h3>Header</h3>
         <h4>{this.context.name}</h4>
+        <div>
+          {counter}<button onClick={this.add}>加1</button>
+        </div>
         <ul>
           {list.map(item => {
             return <li key={item.name}>{item.name}</li>
